c79bb37fb28457631fd6f071a44257df235e10e0
erwin <erwin@erwin-MSI.fritz.box>
2016-05-26 23:07:25 +0200

    studentAdminException added (was forgotten to be committed previously)

---
+++ OpiJ2Opd1/src/studentadmin/StudentAdminException.java
@@ -0,0 +1,12 @@
+package studentadmin;
+
+public class StudentAdminException extends Exception{
+	
+	public StudentAdminException(){
+		super();
+	}
+
+	public StudentAdminException(String message) {
+		super(message);
+	}
+}
